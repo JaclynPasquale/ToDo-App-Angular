1b45247554cb110ab90d5ed647eb3dceef82abf3
jaclynpasquale <jaclynpasquale@gmail.com>
2014-12-05 00:42:04 -0600

    functionality working, installed bootstrap

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+;(function(){
+  'use strict';
+
+  angular.module('myTodoApp', [ ])
+    .controller('TodoAppController', function(){
+    var scope = this;
+
+
+scope.tasks = [
+{
+  name: 'Walk the dog',
+  desc: 'the dog will explode if you don\'t walk it',
+  due: 'tomorrow',
+  priority: 'medium / high',
+  cost: 150.76
+},
+{
+  name: 'Feed the dog',
+  desc: 'the dog will die without food',
+  due: 'today',
+  priority: 'medium',
+  cost: 5
+},
+{
+  name: 'Give the dog a bath',
+  desc: 'the dog smells terrible',
+  due: 'yesterday',
+  priority: 'high',
+  cost: 1907.86
+},
+];
+
+scope.addNewTask = function(){
+  scope.tasks.push(scope.newTask);
+  scope.newTask = null;
+}
+
+scope.removeTask = function(todo){
+  var index = scope.tasks.indexOf(todo);
+  scope.tasks.splice(index, 1);
+}
+
+});
+
+})();

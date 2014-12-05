a958a583b831a4df4471637eba7ecaa0843bba93
jaclynpasquale <jaclynpasquale@gmail.com>
2014-12-05 16:05:09 -0600

    adding more styling with bootstrap and added images to contacts

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 ;(function(){
   'use strict';
 
-  angular.module('addressBook', [ ])
-    .controller('addressBookController', function(){
+  angular.module('AddressBookApp', [ ])
+    .controller('AddressBookController', function(){
     var vm = this;
 
 
@@ -10,52 +10,46 @@ vm.entries = [
 {
   name: 'Mittens McKitten',
   address: '7569 Main St. Columbus, Oh 44060',
-  phone: 550-836-9088,
+  phone: '550-836-9088',
   email: 'mittens@gmail.com',
-  image: 'http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg',
+  image: 'http://instapinch.com/blog/wp-content/uploads/2010/11/kitten-500x500.jpg',
 },
 {
   name: 'Mittens McKitten',
   address: '7569 Main St. Columbus, Oh 44060',
-  phone: 550-836-9088,
+  phone: '550-836-9088',
   email: 'mittens@gmail.com',
-  image: 'http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg',
+  image: 'http://www.omghowcheap.co.uk/ekmps/shops/omghowcheap/images/birthday-card-blank-little-cat-design-square-size-5.25-x-5.25-by-ling-ff0555-5434-p.jpg',
 },
 {
   name: 'Mittens McKitten',
   address: '7569 Main St. Columbus, Oh 44060',
-  phone: 550-836-9088,
+  phone: '550-836-9088',
   email: 'mittens@gmail.com',
-  image: 'http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg',
+  image: 'http://www.omghowcheap.co.uk/ekmps/shops/omghowcheap/images/birthday-card-blank-kitten-design-large-square-size-6.25-x-6.25-hi0160-5476-p.jpg',
 },
 ];
 
-scope.addNewTask = function(){
-  scope.tasks.push(scope.newTask);
-  scope.newTask = _freshTask();
+vm.addNewEntry = function(){
+  vm.entries.push(vm.newEntry);
+  vm.newEntry = _freshEntry();
 };
 
 
 
-scope.removeTask = function(todo){
-  var index = scope.tasks.indexOf(todo);
-  scope.tasks.splice(index, 1);
+vm.removeEntry = function(AddressBook){
+  var index = vm.entries.indexOf(AddressBook);
+  vm.entries.splice(index, 1);
 };
 
-scope.newTask = _freshTask();
+vm.newEntry = _freshEntry();
 
-scope.priorityOptions = {
-  high: 'High',
-  medium: 'Medium',
-  low: 'Low',
-  notimportant: 'Meh'
-};
+function _freshEntry(){
+  return {
 
-function _freshTask(){
-  return{
-    priority: 'medium'
   }
 }
+
 
 });
 
